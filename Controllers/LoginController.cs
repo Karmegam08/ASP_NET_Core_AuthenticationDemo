9203f6cfe0f16d444603adffbe42b6b05e04068e
Karmegam <103259299+karmegams02@users.noreply.github.com>
2024-09-10 23:54:05 +0530

    Added the ASP NET CORE Sample

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using CookieAuthDemo.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using System.Diagnostics;
+namespace CookieAuthDemo.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Index(string username, string password)
+        {
+            var user = UserStore.ValidateUser(username, password);
+            if (user != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName)
+                };
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity));
+                return RedirectToAction("Index", "Home");
+            }
+            ViewBag.ErrorMessage = "Invalid username or password";
+
+            return Error();
+        }
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿namespace CookieAuthDemo.Models
+{
+    public class User
+    {
+        public string Password { get; set; }
+        public string UserName { get; set; }
+    }
+    public static class UserStore
+    {
+        public static List<User> Users = new List<User>
+    {
+        new User { UserName = "testuser", Password = "password123" }  // Sample user
+    };
+
+        public static User ValidateUser(string username, string password)
+        {
+            return Users.FirstOrDefault(u => u.UserName == username && u.Password == password);
+        }
+    }
+
+}
